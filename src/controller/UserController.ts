9f3f97014bb3695fbf9adfad8cd77091a2ddf9f9
NhaTD <nhatran406@gmail.com>
2018-07-13 16:48:25 +0700

    ds

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -138,27 +138,195 @@ export class UserController {
         await this.userService.remove(request.params.id);
     }
 
+    @httpPut("/comment")
+    public async createComment(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.create("comment", request);
+        return data;
+    }
+
+    @httpPost("/:id/comment")
+    public async updateComment(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.update("comment", request);
+        return data;
+    }
+
+    @httpGet("/comment/byid/:id")
+    public async findCommentById(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.findById("comment", request);
+        return data;
+    }
+
+    @httpGet("/comment/search")
+    public async searchComment(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.search("comment", request);
+        return data;
+    }
+
     @httpPut("/project")
-    public async createProperty(request: Request, response: Response, next: NextFunction) {
+    public async createProject(request: Request, response: Response, next: NextFunction) {
         const data = await this.callService.create("project", request);
-        return this.utils.createSuccessHandler(data);
+        return data;
     }
 
     @httpPost("/:id/project")
-    public async updateProperty(request: Request, response: Response, next: NextFunction) {
+    public async updateProject(request: Request, response: Response, next: NextFunction) {
         const data = await this.callService.update("project", request);
-        return this.utils.createSuccessHandler(data);
+        return data;
     }
 
     @httpGet("/project/byid/:id")
-    public async findPropertyById(request: Request, response: Response, next: NextFunction) {
+    public async findProjectById(request: Request, response: Response, next: NextFunction) {
         const data = await this.callService.findById("project", request);
-        return this.utils.createSuccessHandler(data);
+        return data;
     }
 
     @httpGet("/project/search")
-    public async searchProperty(request: Request, response: Response, next: NextFunction) {
+    public async searchProject(request: Request, response: Response, next: NextFunction) {
         const data = await this.callService.search("project", request);
-        return this.utils.createSuccessHandler(data);
+        return data;
+    }
+
+    @httpPut("/property")
+    public async createProperty(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.create("property", request);
+        return data;
+    }
+
+    @httpPost("/:id/property")
+    public async updateProperty(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.update("property", request);
+        return data;
+    }
+
+    @httpGet("/property/byid/:id")
+    public async findPropertyById(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.findById("property", request);
+        return data;
+    }
+
+    @httpGet("/property/search")
+    public async searchProperty(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.search("property", request);
+        return data;
+    }
+
+    @httpPut("/interested")
+    public async createInterested(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.create("interested", request);
+        return data;
+    }
+
+    @httpPost("/:id/interested")
+    public async updateInterested(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.update("interested", request);
+        return data;
+    }
+
+    @httpGet("/interested/byid/:id")
+    public async findInterestedById(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.findById("interested", request);
+        return data;
+    }
+
+    @httpGet("/interested/search")
+    public async searchInterested(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.search("interested", request);
+        return data;
+    }
+
+    @httpPut("/propertytype")
+    public async createPropertytype(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.create("propertytype", request);
+        return data;
+    }
+
+    @httpPost("/:id/propertytype")
+    public async updatePropertytype(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.update("propertytype", request);
+        return data;
+    }
+
+    @httpGet("/propertytype/byid/:id")
+    public async findPropertytypeById(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.findById("propertytype", request);
+        return data;
+    }
+
+    @httpGet("/propertytype/search")
+    public async searchPropertytype(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.search("propertytype", request);
+        return data;
+    }
+
+    @httpPut("/relation")
+    public async createRelation(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.create("relation", request);
+        return data;
+    }
+
+    @httpPost("/:id/relation")
+    public async updateRelation(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.update("relation", request);
+        return data;
+    }
+
+    @httpGet("/relation/byid/:id")
+    public async findRelationById(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.findById("relation", request);
+        return data;
+    }
+
+    @httpGet("/relation/search")
+    public async searchRelation(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.search("relation", request);
+        return data;
+    }
+
+    @httpPut("/report")
+    public async createReport(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.create("report", request);
+        return data;
+    }
+
+    @httpPost("/:id/report")
+    public async updateReport(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.update("report", request);
+        return data;
+    }
+
+    @httpGet("/report/byid/:id")
+    public async findReportById(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.findById("report", request);
+        return data;
+    }
+
+    @httpGet("/report/search")
+    public async searchReport(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.search("report", request);
+        return data;
+    }
+
+    @httpPut("/transaction")
+    public async createTransaction(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.create("transaction", request);
+        return data;
+    }
+
+    @httpPost("/:id/transaction")
+    public async updateTransaction(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.update("transaction", request);
+        return data;
+    }
+
+    @httpGet("/transaction/byid/:id")
+    public async findTransactionById(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.findById("transaction", request);
+        return data;
+    }
+
+    @httpGet("/transaction/search")
+    public async searchTransaction(request: Request, response: Response, next: NextFunction) {
+        const data = await this.callService.search("transaction", request);
+        return data;
     }
 }
